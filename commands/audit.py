7785339eb796f7c9fed3cba9dc9c21248c984a1b
Jean-Marc Fontaine <jmf@spacelift.io>
2023-09-21 18:04:40 -0400

    WIP

--- commands/audit.py
+++ commands/audit.py
@@ -2,16 +2,19 @@ import click
 
 from importlib import import_module
 
+
 def _import_class_from_string(path):
-    module_path, _, class_name = path.rpartition('.')
+    module_path, _, class_name = path.rpartition(".")
     mod = import_module(module_path)
     klass = getattr(mod, class_name)
 
     return klass
 
-def _load_exporter(name, settings={}):
+
+def _load_exporter(console, name, settings={}):
     klass = _import_class_from_string(f"exporters.{name}.Exporter")
-    return klass(**settings)
+    return klass(console, **settings)
+
 
 @click.command()
 @click.pass_context
@@ -19,6 +22,7 @@ def audit(ctx):
     config = ctx.obj["config"]
     exporter_settings = config["exporter"]
     exporter_name = exporter_settings.pop("name")
-    exporter = _load_exporter(exporter_name, exporter_settings)
+    exporter = _load_exporter(
+        console=ctx.obj["console"], name=exporter_name, settings=exporter_settings
+    )
     exporter.audit()
-
