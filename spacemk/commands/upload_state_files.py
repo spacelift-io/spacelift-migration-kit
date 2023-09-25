0c95e43577292ccb436bc7f49d3687059e1b75e7
Jean-Marc Fontaine <jmf@spacelift.io>
2023-09-25 18:09:28 -0400

    WIP

--- spacemk/commands/upload_state_files.py
+++
@@ -1,9 +0,0 @@
-import click
-
-
-@click.command(help="Upload Terraform state files to state backend.")
-@click.pass_context
-def upload_state_files(ctx):
-    console = ctx.obj["console"]
-
-    console.print("This command has not been implemented yet.")
