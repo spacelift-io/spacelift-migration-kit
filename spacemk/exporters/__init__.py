2fe6ee038157637c99bd099472b5a1668c12f395
Jean-Marc Fontaine <jmf@spacelift.io>
2023-09-26 17:40:11 -0400

    WIP

--- spacemk/exporters/__init__.py
+++ spacemk/exporters/__init__.py
@@ -4,9 +4,8 @@ from importlib import import_module
 def _import_class_from_string(path):
     module_path, _, class_name = path.rpartition(".")
     mod = import_module(module_path)
-    klass = getattr(mod, class_name)
 
-    return klass
+    return getattr(mod, class_name)
 
 
 def load_exporter(config, console):
