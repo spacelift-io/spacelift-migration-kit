747509f141928de1e9e435312f81f416f3b122b0
Jean-Marc Fontaine <jmf@spacelift.io>
2023-10-12 20:20:51 -0400

    Change test names

--- tests/test_exporter_terraform.py
+++ tests/test_exporter_terraform.py
@@ -5,62 +5,62 @@ from spacemk.exporters import terraform
 
 
 @pytest.mark.vcr()
-def test_tfe_agent_pools(terraform_exporter: terraform.Exporter):
+def test_tfe_list_agent_pools(terraform_exporter: terraform.Exporter):
     terraform_exporter._list_agent_pools("org-example")
 
 
 @pytest.mark.vcr()
-def test_tfe_organization(terraform_exporter: terraform.Exporter):
+def test_tfe_list_organization(terraform_exporter: terraform.Exporter):
     organizations = terraform_exporter._list_organizations()
     assert len(organizations) == 1
     assert organizations[0]["attributes"]["id"] == "org-example"
 
 
 @pytest.mark.vcr()
-def test_tfe_policies(terraform_exporter: terraform.Exporter):
+def test_tfe_list_policies(terraform_exporter: terraform.Exporter):
     terraform_exporter._list_policies("org-example")
 
 
 @pytest.mark.vcr()
-def test_tfe_policy_sets(terraform_exporter: terraform.Exporter):
+def test_tfe_list_policy_sets(terraform_exporter: terraform.Exporter):
     terraform_exporter._list_policy_sets("org-example")
 
 
 @pytest.mark.vcr()
-def test_tfe_projects(terraform_exporter: terraform.Exporter):
+def test_tfe_list_projects(terraform_exporter: terraform.Exporter):
     terraform_exporter._list_projects("org-example")
 
 
 @pytest.mark.vcr()
-def test_tfe_registry_modules(terraform_exporter: terraform.Exporter):
+def test_tfe_list_registry_modules(terraform_exporter: terraform.Exporter):
     terraform_exporter._list_registry_modules("org-example")
 
 
 @pytest.mark.vcr()
-def test_tfe_registry_providers(terraform_exporter: terraform.Exporter):
+def test_tfe_list_registry_providers(terraform_exporter: terraform.Exporter):
     terraform_exporter._list_registry_providers("org-example")
 
 
 @pytest.mark.vcr()
-def test_tfe_tasks(terraform_exporter: terraform.Exporter):
+def test_tfe_list_tasks(terraform_exporter: terraform.Exporter):
     terraform_exporter._list_tasks("org-example")
 
 
 @pytest.mark.vcr()
-def test_tfe_teams(terraform_exporter: terraform.Exporter):
+def test_tfe_list_teams(terraform_exporter: terraform.Exporter):
     terraform_exporter._list_teams("org-example")
 
 
 @pytest.mark.vcr()
-def test_tfe_variables(terraform_exporter: terraform.Exporter):
+def test_tfe_list_variables(terraform_exporter: terraform.Exporter):
     terraform_exporter._list_variables("ws-example")
 
 
 @pytest.mark.vcr()
-def test_tfe_variable_sets(terraform_exporter: terraform.Exporter):
+def test_tfe_list_variable_sets(terraform_exporter: terraform.Exporter):
     terraform_exporter._list_variable_sets("org-example")
 
 
 @pytest.mark.vcr()
-def test_tfe_workspaces(terraform_exporter: terraform.Exporter):
+def test_tfe_list_workspaces(terraform_exporter: terraform.Exporter):
     terraform_exporter._list_workspaces("org-example")
