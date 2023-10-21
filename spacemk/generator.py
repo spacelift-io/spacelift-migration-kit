5ebf6d27cb9194d99e45df546ee103afee97aa1b
Jean-Marc Fontaine <jmf@spacelift.io>
2023-10-21 16:38:37 -0400

    Improve generator templating

--- spacemk/generator.py
+++ spacemk/generator.py
@@ -1,7 +1,22 @@
 import json
 from pathlib import Path
 
-from jinja2 import Environment, PackageLoader, select_autoescape
+from jinja2 import Environment, PackageLoader, nodes
+from jinja2.exceptions import TemplateRuntimeError
+from jinja2.ext import Extension
+
+
+class RaiseExtension(Extension):
+    tags = set(["raise"])  # noqa: RUF012
+
+    def parse(self, parser):
+        lineno = next(parser.stream).lineno
+        message_node = parser.parse_expression()
+
+        return nodes.CallBlock(self.call_method("_raise", [message_node], lineno=lineno), [], [], [], lineno=lineno)
+
+    def _raise(self, msg, caller):  # noqa: ARG002
+        raise TemplateRuntimeError(msg)
 
 
 class Generator:
@@ -9,8 +24,18 @@ class Generator:
         self._config = config
         self._console = console
 
+    def _filter_totf(self, value):
+        return json.dumps(value, ensure_ascii=False)
+
     def _generate_code(self, data: dict):
-        env = Environment(loader=PackageLoader("spacemk"), autoescape=select_autoescape())
+        env = Environment(
+            autoescape=False,
+            extensions=[RaiseExtension],
+            loader=PackageLoader("spacemk"),
+            lstrip_blocks=True,
+            trim_blocks=True,
+        )
+        env.filters["totf"] = self._filter_totf
         template = env.get_template("main.tf.jinja")
         content = template.render(**data)
         self._save_to_file("main.tf", content)
