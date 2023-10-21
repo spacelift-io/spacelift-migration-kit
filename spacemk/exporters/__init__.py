a0ffde92cdb83d977709f336dec6e749ab536168
Jean-Marc Fontaine <jmf@spacelift.io>
2023-10-21 17:42:01 -0400

    Improve configuration handling

--- spacemk/exporters/__init__.py
+++ spacemk/exporters/__init__.py
@@ -3,15 +3,19 @@ from importlib import import_module
 
 def _import_class_from_string(path):
     module_path, _, class_name = path.rpartition(".")
-    mod = import_module(module_path)
+    try:
+        mod = import_module(module_path)
+    except ModuleNotFoundError as e:
+        raise ValueError("Exporter name is incorrect") from e
 
     return getattr(mod, class_name)
 
 
 def load_exporter(config, console):
-    name = config.get("name")
-    settings = config.get("settings", {})
+    if not config.get("name"):
+        raise ValueError("Exporter name is missing")
 
+    name = config.get("name")
     klass = _import_class_from_string(f"spacemk.exporters.{name}.Exporter")
 
-    return klass(console, **settings)
+    return klass(console=console, config=config.get("settings", {}))
