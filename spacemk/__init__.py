7809dfae5eeac695439fb18bd2d051f7eac82f90
Jean-Marc Fontaine <jmf@spacelift.io>
2023-11-02 18:17:32 -0400

    Export the default branch name from Terraform

--- spacemk/__init__.py
+++ spacemk/__init__.py
@@ -2,7 +2,7 @@ import subprocess
 from shutil import which
 
 
-def is_command_available(command: str, execute: bool = False) -> bool:
+def is_command_available(command: str | list[str], execute: bool = False) -> bool:
     if execute:
         try:
             return subprocess.run(command, capture_output=True, check=False).returncode == 0
