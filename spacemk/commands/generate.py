3171c061a17094f7cf21dba2b3342677f0a789a6
Jean-Marc Fontaine <jmf@spacelift.io>
2023-11-04 12:28:21 -0400

    Pass command config as metadata instead of context object

--- spacemk/commands/generate.py
+++ spacemk/commands/generate.py
@@ -1,12 +1,11 @@
 import click
+from click.decorators import pass_meta_key
 
 from spacemk.generator import Generator
 
 
 @click.command(help="Generate Terraform code to manage Spacelift entities.")
-@click.pass_context
-def generate(ctx):
-    config = ctx.obj["config"]
-
-    generator = Generator(config=config.get("generator", {}))
+@pass_meta_key("config")
+def generate(config):
+    generator = Generator(config.get("generator", {}))
     generator.generate()
