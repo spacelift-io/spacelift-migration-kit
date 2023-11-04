a80b4309ab4655e4641837ec17d60ccf60af007e
Jean-Marc Fontaine <jmf@spacelift.io>
2023-11-04 09:58:00 -0400

    Dynamically load default commands

--- spacemk/commands/__init__.py
+++
@@ -1,8 +0,0 @@
-# ruff: noqa: F401
-from .audit import audit
-from .clean import clean
-from .config import config
-from .export import export
-from .generate import generate
-
-# from .migrate import migrate # noqa: ERA001
