243f1637c8821edbb29ea9558d4e27834a861561
Jean-Marc Fontaine <jmf@spacelift.io>
2023-11-04 17:41:37 -0400

    Delete unused code

--- spacemk/state_uploader.py
+++
@@ -1,7 +0,0 @@
-class StateUploader:
-    def __init__(self, config):
-        self._config = config
-
-    def upload_state_files(self):
-        """Upload Terraform state files to state backend"""
-        pass
