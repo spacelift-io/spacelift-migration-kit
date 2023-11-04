b46a57406085eeebffc571ef1b94747fc9822a48
Jean-Marc Fontaine <jmf@spacelift.io>
2023-11-04 17:37:14 -0400

    Allow custom exporters

--- spacemk/exporters/__init__.py
+++ spacemk/exporters/__init__.py
@@ -1,13 +1,44 @@
-import sys
+import logging
+from importlib.machinery import SourceFileLoader
+from pathlib import Path
+from typing import Any
 
-from .terraform import TerraformExporter  # noqa: F401
+from spacemk.exporters.base import BaseExporter
+
+
+def _get_exporter_class(exporter_name: str, path: Path) -> Any:
+    class_name = f"{exporter_name.title()}Exporter"
+    module = SourceFileLoader("exporters", path.as_posix()).load_module()
+
+    if not hasattr(module, class_name):
+        raise Exception(f"Could not find '{class_name}' custom exporter class in '{path}'")  # noqa: TRY002
+
+    class_ = getattr(module, class_name)
+
+    if not issubclass(class_, BaseExporter):
+        raise TypeError(f"'{class_name}' custom exporter class does not derive from the 'spacemk.BaseExporter' class")
+
+    return class_
 
 
 def load_exporter(config):
-    if not config.get("name"):
+    exporter_name = config.get("name")
+
+    if not exporter_name:
         raise ValueError("Exporter name is missing")
 
-    class_name = f"{config.get('name').title()}Exporter"
-    class_ = getattr(sys.modules[__name__], class_name)
+    current_file_path = Path(__file__).parent.resolve()
+    native_exporter_path = Path(current_file_path, f"./{exporter_name}.py").resolve()
+    if native_exporter_path.exists():
+        logging.debug(f"Loading native '{exporter_name}' exporter")
+        class_ = _get_exporter_class(exporter_name=exporter_name, path=native_exporter_path)
+    else:
+        logging.debug(f"Could not find native '{exporter_name}' exporter. Looking for a customer exporter.")
+        custom_exporter_path = Path(current_file_path, f"../../custom/exporters/{exporter_name}.py").resolve()
+        if custom_exporter_path.exists():
+            logging.debug(f"Loading custom '{exporter_name}' exporter")
+            class_ = _get_exporter_class(exporter_name=exporter_name, path=custom_exporter_path)
+        else:
+            raise Exception(f"Could not find '{exporter_name}' exporter file")  # noqa: TRY002
 
     return class_(config=config.get("settings", {}))
