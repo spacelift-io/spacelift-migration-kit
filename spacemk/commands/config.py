1acd5945c2e11f54a02f30f2d37e9205332c8f2f
Jean-Marc Fontaine <jmf@spacelift.io>
2023-11-04 14:24:02 -0400

    Remove unimplemented config command for now

--- spacemk/commands/config.py
+++
@@ -1,7 +0,0 @@
-import click
-
-
-@click.command(help="Generate configuration file.")
-def config():
-    click.echo("This command has not been implemented yet.")
-    click.echo("For the time being, copy the 'config.yml.example' file to 'config.yml' and edit it.")
