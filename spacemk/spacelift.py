a7081c859e4dc6f8c8f0e4a1e40d7146bfa9e29d
Jean-Marc Fontaine <jmf@spacelift.io>
2023-11-24 19:04:26 -0500

    Create module versions

--- spacemk/spacelift.py
+++ spacemk/spacelift.py
@@ -2,6 +2,7 @@ import logging
 
 import requests
 from benedict import benedict
+from requests_toolbelt.utils import dump as request_dump
 
 from spacemk import load_normalized_data
 
@@ -17,13 +18,23 @@ class Spacelift:
         self._api_jwt_token = None
 
     def _call_api(self, operation: str, variables: dict | None = None) -> dict:
-        response = requests.post(
-            headers={"Authorization": f"Bearer {self._get_api_jwt_token()}"},
-            json={"query": operation, "variables": variables},
-            url=self._config.get("api.api_key_endpoint"),
-        )
+        try:
+            response = requests.post(
+                headers={"Authorization": f"Bearer {self._get_api_jwt_token()}"},
+                json={"query": operation, "variables": variables},
+                url=self._config.get("api.api_key_endpoint"),
+            )
+            logging.debug(request_dump.dump_all(response).decode("utf-8"))
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            raise RuntimeError(f"HTTP Error: {e}") from e
 
-        return benedict(response.json())
+        data = benedict(response.json())
+
+        if "errors" in data:
+            logging.warning(data.get("errors[0].message"))
+
+        return data
 
     def _get_api_jwt_token(self) -> str:
         if not self._api_jwt_token:
@@ -96,6 +107,60 @@ class Spacelift:
                 f"'{env_var.get('_relationships.stack.slug')}/{env_var.get('name')}': "
                 f"{response.get('errors[0].message')}"
             )
+
+    def _create_module_version(self, commit_sha: str, module: str, version: str) -> None:
+        operation = """
+          mutation CreateModuleVersion($moduleId: ID!, $commitSha: String, $version: String) {
+            versionCreate(module: $moduleId, commitSha: $commitSha, version: $version) {
+              id
+            }
+          }
+        """
+
+        variables = {
+            "commitSha": commit_sha,
+            "moduleId": module,
+            "version": version,
+        }
+
+        self._call_api(operation=operation, variables=variables)
+
+    def _get_module_versions(self, module: str) -> list:
+        versions = {}
+
+        operation = """
+          query GetModuleVersions($moduleId: ID!, $before: ID, $includeFailed: Boolean) {
+            module(id: $moduleId) {
+              id
+              versions(before: $before, includeFailed: $includeFailed) {
+                commit {
+                  hash
+                }
+                number
+              }
+            }
+          }
+        """
+
+        variables = {
+            "includeFailed": False,
+            "moduleId": module,
+        }
+
+        response = self._call_api(operation=operation, variables=variables)
+        for version in response.get("data.module.versions"):
+            versions[version["number"]] = version["commit"]["hash"]
+
+        return versions
+
+    def create_module_version(self, commit_sha: str, module: str, version: str):
+        versions = self._get_module_versions(module=module)
+
+        if version in versions:
+            logging.info(f"Version '{version}' of module '{module}' already exists. Skipping.")
+        else:
+            self._create_module_version(commit_sha=commit_sha, module=module, version=version)
+            logging.debug(f"Created version '{version}' using commit '{commit_sha}' for module '{module}'")
 
     def set_sensitive_env_vars(self) -> None:
         env_vars = self._get_sensitive_env_vars()
